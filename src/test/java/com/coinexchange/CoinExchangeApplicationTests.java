860311f1eb35ad520d764f6c0f32dcb079014002
dudxor4587 <dudxor4587@gmail.com>
2025-05-11 19:25:08 +0900

    first commit

---
+++ src/test/java/com/coinexchange/CoinExchangeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coinexchange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoinExchangeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
